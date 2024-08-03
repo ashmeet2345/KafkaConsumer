04018fbdb94bf96272802a3f90eae86753bf08bb
ashmeet420-69 <ashmeets424@gmail.com>
2024-08-03 17:16:59 +0530

    Specific partition routing

--- src/main/java/com/example/consumer/KafkaMessageListener.java
+++ src/main/java/com/example/consumer/KafkaMessageListener.java
@@ -4,6 +4,7 @@ import com.example.dto.Customer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,9 +12,10 @@ public class KafkaMessageListener {
 
     Logger log = LoggerFactory.getLogger(KafkaMessageListener.class);
 
-    @KafkaListener(topics = "newTopic",groupId = "group-a")
-    public void consumeEvents(Customer customer) {
-        log.info("consumer consume the events {} ", customer.toString());
+    @KafkaListener(topics = "newKafkaTopic",groupId = "group-a",
+            topicPartitions = {@TopicPartition(topic = "newKafkaTopic",partitions = {"4"})})
+    public void consumeEvents(String message) {
+        log.info("consumer consume the events {} ", message);
     }
 
 //    @KafkaListener(topics = "javatechie-demo1",groupId = "jt-group-new")
